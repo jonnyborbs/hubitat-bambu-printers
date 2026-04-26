85073907c7d76a7f4c5040e8ca17e14cf9600b71
Jon Schulman <25127543+jonnyborbs@users.noreply.github.com>
2026-04-26 09:45:46 -0700

    reverting loop and interval control behavior

--- bambulab-printer-driver.groovy
+++ bambulab-printer-driver.groovy
@@ -494,8 +494,8 @@ def lightOn() {
             led_mode:      "on",
             led_on_time:   500,
             led_off_time:  500,
-            loop_times:    1,
-            interval_time: 1000
+            loop_times:    0,
+            interval_time: 0
         ]
     ])
     sendEvent(name: "chamberLight", value: "on")
@@ -512,8 +512,8 @@ def lightOff() {
             led_mode:      "off",
             led_on_time:   500,
             led_off_time:  500,
-            loop_times:    1,
-            interval_time: 1000
+            loop_times:    0,
+            interval_time: 0
         ]
     ])
     sendEvent(name: "chamberLight", value: "off")
